aa2e0b25bc27a07f34ffd9c83164197d0e4c7d5a
A kwamena <kwamenadollar17@yahoo.com>
2025-04-02 06:20:46 -0400

    webhook test

--- main.tf
+++ main.tf
@@ -9,6 +9,17 @@ resource "aws_instance" "project-web-server" {
   }
 }
 
+#Creating web server for airbnb
+
+resource "aws_instance" "project-dev-server" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "project-dev-server"
+  }
+}
+
 #Creating prod server for airbnb
 resource "aws_instance" "project-prod-server" {
   ami           =var.ami_id
